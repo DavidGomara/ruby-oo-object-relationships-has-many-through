cf2f0f8f770c5c168185b7e821cad1436f1c48da
David Gomara <davidgomara7@gmail.com>
2020-11-30 03:30:11 +0000

    commit tree 677602145a13f0b2daec09b621e225c626ea04a0

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
-
+    
+    attr_accessor :experience
+    attr_reader :name 
+    
+    @@all = []
+    
+    def initialize(name, experience)
+        @name = name 
+        @experience = experience
+        @@all << self
+    end 
+    
+    def self.all
+        @@all
+    end 
+    
+    def new_meal(customer, total, tip)
+        new_meal = Meal.new(self, customer, total, tip)
+        new_meal
+    end 
+    
+    def meals
+        Meal.all.select { |meal| meal.waiter == self}
+    end 
+    
+    def best_tipper
+        self.meals.sort_by { |meal| meal.tip }.last.customer
+    end 
+    
 end
